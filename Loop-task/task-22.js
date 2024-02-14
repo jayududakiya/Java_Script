b76d02d931e7eb1919b5de28ac0641e54ba4b110
JD <137084651+jayududakiya@users.noreply.github.com>
2024-02-14 16:02:55 +0530

    Topic

--- Loop-task/task-22.js
+++ Loop-task/task-22.js
@@ -1,14 +1,28 @@
 "use strict"
+// {
+//     let string = "";
+//     for(let i = 0 ; i <= 5 ; i++) 
+//     {
+//         for(let j = 0 ; j <= 5 ; j++) 
+//         {
+//             string = string + i ;
+//             // console.log( string + "\n" );
+//         }
+//         console.log( string + "\n" );
+//     }
+//     console.log(string );
+// }
+
 {
-    let string = "";
-    for(let i = 0 ; i <= 5 ; i++) 
-    {
-        for(let j = 0 ; j <= 5 ; j++) 
-        {
-            string = string + i ;
-            // console.log( string + "\n" );
-        }
-        console.log( string + "\n" );
-    }
-    console.log(string );
+
+function Random () {
+    let Ran ;
+    Ran = Number.parseInt(Math.random()*3);
+    return Ran;
+}
+
+let x = Random();
+
+let Get = prompt(`Chose any of Number below : \n 1 . To Stone \n 2 . To Paper \n 3 . Scissor`,);
+console.log(Get);
 }
