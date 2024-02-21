3efab37058aa2cc235619b270abd3ff09c193bd5
JD <137084651+jayududakiya@users.noreply.github.com>
2024-02-21 15:13:23 +0530

    21/02/2024

--- String/script.js
+++ String/script.js
@@ -114,5 +114,172 @@ Newpad = padNum1 + " " + padNum ;
 
 document.write(Newpad); 
 
+}
+
+{
+    console.log(`this is Slice() Method`);
+
+    let text = "Hello This Javascript Language And This is Programming Language";
+
+    console.log(text);
+
+    let Up_text = text.slice(0,5); // this is start with 0 index To 5 index letter print 
+
+    
+    console.log(">", Up_text);
+    
+    Up_text = text.slice(10);  // this is start with 10 index To Lsat index letter print 
+    
+    console.log(">", Up_text);
+                                    // print in revers 
+    Up_text = text.slice(-10,-4);  // this is start with last index of string and Go to given number index letter print 
+
+    Up_text = text.slice(-4);  // this is start with -4 and end with -1 value 
+    
+    console.log(">", Up_text);
+
+}
+
+{
+    console.log(`this is charAt() Method`);
+
+    let text = "Hello This Javascript Language And This is Programming Language";
+
+    console.log(text);
+
+    
+    let New_text = text.charAt(0); // this only print letter on give index number 
+    
+    console.log(New_text);
+
+}
+
+{
+    console.log(`this is charcodeAt() Method`);
+
+    let text = "Hello This Javascript Language And This is Programming Language";
+
+    console.log(text); // J == 11 index  // J == 74 ascii value 
+
+    let New_text = text.charCodeAt(3); // this only print ascii value of give index number 
+
+    console.log(New_text);
+
+}
+
+{
+    let i = 65;
+    let x = "";
+    console.log("Alphabets form (A-Z) are:");
+    document.write(`<br> <br> <h1> Alphabets form (A-Z) are : </h1>`);
+ 
+    while (i <= 90) {
+        x = x +" "+ String.fromCharCode(i);
+        i++;
+    }
+
+    console.log(x);
+    document.write(` <br> <pre> ${x} </pre>`);
+}
+
+
+{
+    console.log(`this is subString() Method`);
+
+    let text = "Hello This Javascript Language And This is Programming Language";
+
+    console.log(text);
+
+    let New_text = text.substring(1,16); 
+
+    console.log(">",New_text);
+
+    New_text = text.substring(-1,6);
+
+    console.log(">",New_text);
+
+    New_text = text.substring(-1);
+
+    console.log(">",New_text);
+
+    New_text = text.substring(-1,-6);
+
+    console.log(">",New_text);
+
+}
+
+{
+    console.log(`this is substr() Method`);
+
+    let text = "Hello This Javascript Language And This is Programming Language";
+
+    console.log(text);
+
+    let New_text = text.substr(0,16);  // this method counts words
+
+    console.log("16 words >",New_text);
+
+    New_text = text.substr(18);
+
+    console.log("18 words>",New_text);
+
+    New_text = text.substr(-8); // this take only one number in negative value 
+
+    console.log("last 8 words>",New_text);
+
+    New_text = text.substr(-1,-6); // this is don't print any sting 
+
+    console.log(" 0 words >",New_text);
+
+}
+
+
+{
+
+    console.log(`this is replace() Method`);
+
+    // this method is replace only first same word 
+
+    let text = "Hello This Javascript Language And This is Programming Language";
+
+    console.log(text);
+
+    let New_word = text.replace("Hello" , " Hii ");
+
+    console.log(">" , New_word);
+
+}
+
+{
+
+    console.log(`this is replaceAll() Method`);
+
+    // this method is replace all same word 
+
+    let text = "Hello This Javascript Language And This is Programming Language";
+
+    console.log(text);
+
+    let New_word = text.replaceAll("This" , "'Hii'");
+
+    console.log(">" , New_word);
+
+}
+
+
+{
+
+    console.log(`this is replace() with '/g' key word Method`);
+
+    // this method is replace only first same word 
+
+    let text = "Hello This Javascript language And This is Programming language";
+
+    // console.log(text);
+
+    let New_word = text.replace(/l/g , "'L'");
+
+    console.log("> " , New_word);
+    document.write("<br> > " , New_word , "<br> <br>");
 
 }
