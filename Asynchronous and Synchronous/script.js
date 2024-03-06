c342871e439446a7bd024ec2235b230e8c9ad58f
JD <137084651+jayududakiya@users.noreply.github.com>
2024-03-06 17:30:26 +0530

    Topic / 06/03/2024

---
+++ Asynchronous and Synchronous/script.js
@@ -0,0 +1,19 @@
+// setTimeout();
+
+{
+    function hello (name, lastname) {
+        console.log('Hello ' + name + ' ' + lastname + ' ' + 'Its-J');
+    }
+
+    function hello1 () {
+        console.log('hello world 2')
+    }
+
+    function hello2 () {
+        console.log('hello world 3')
+    }
+    
+    setTimeout(hello,2000, 'jayan', 'dudakiya');
+
+    // hello();
+}
---
+++ Cello and Deep copy/script.js
@@ -0,0 +1,55 @@
+{
+    // cello copy and deep copy 
+    // rest parameters
+
+
+    console.log(`Cello & Deep copy using Rest parameters`.toUpperCase());
+
+    console.log(`=> In Array`);
+
+    let MyProducts = ['Tv','Watch','Mobile','Laptop'];
+
+    // with out rest parameters  cello copy
+
+    let NewProducts = MyProducts;
+
+    // with rest parameters deep copy
+    
+    //  NewProducts = [...MyProducts];
+    
+    // change in array 
+    NewProducts.push('Tablets');
+    NewProducts.push('SmartTv');
+    
+    console.log(`=> this MyProducts`, MyProducts);
+    console.log(`=> this NewProducts`, NewProducts);
+}
+
+
+{
+    console.log(`\nCello & Deep copy using Rest parameters`.toUpperCase());
+    
+    console.log(`=> In Object`);
+    
+    let userAccount = {
+        Name : 'harry potter',
+        Email : 'harrypotter@gmail.com',
+        Age : 35,
+        InstaId : 'harry_21#'
+    }
+    
+    // with out rest parameters  cello copy
+    let updateAccount = userAccount;
+    userAccount.gender = 'Male';
+    
+    // with rest parameters deep copy in object must use {} 
+    updateAccount = {...userAccount};
+
+
+    // change in object
+    updateAccount.whtasappNum = 3132332323;
+
+    console.log(userAccount);
+    console.log(updateAccount);
+
+}
